ea6a5b3b71007e005a1aa9152e5198698135cb52
Junke Zhu <brucezhu712@gmail.com>
2025-04-02 12:59:44 -0700

    initialization

---
+++ src/main/java/com/group12/taskscheduler/TaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.group12.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerAppApplication.class, args);
+    }
+
+}
