2c19870fb3282c1fae224e2a11f8c6212fa0b0e6
Junke Zhu <brucezhu712@gmail.com>
2025-04-02 16:07:53 -0700

    some basic setups

--- src/main/java/com/group12/taskscheduler/TaskSchedulerApplication.java
+++ src/main/java/com/group12/taskscheduler/TaskSchedulerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskSchedulerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskSchedulerAppApplication.class, args);
+        SpringApplication.run(TaskSchedulerApplication.class, args);
     }
 
 }
